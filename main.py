87ae0895300b722771848f1582f0c599aa321ecb
Law <wilbertradgarcia1221@gmail.com>
2023-03-16 00:22:54 +0900

    Initial commit

---
+++ main.py
@@ -0,0 +1,93 @@
+from fastapi import FastAPI
+from fastapi.params import Body
+from newspaper import Article
+import re
+from nltk.corpus import stopwords
+from urllib.parse import urlparse
+
+stop = set(stopwords.words('english'))
+alphabets= "([A-Za-z])"
+prefixes = "(Mr|St|Mrs|Ms|Dr)[.]"
+suffixes = "(Inc|Ltd|Jr|Sr|Co)"
+starters = "(Mr|Mrs|Ms|Dr|Prof|Capt|Cpt|Lt|He\s|She\s|It\s|They\s|Their\s|Our\s|We\s|But\s|However\s|That\s|This\s|Wherever)"
+acronyms = "([A-Z][.][A-Z][.](?:[A-Z][.])?)"
+websites = "[.](com|net|org|io|gov|edu|me)"
+digits = "([0-9])"
+
+def remove_html(text):
+    html_pattern = re.compile('<.*?>')
+    return html_pattern.sub(r'', text)
+
+def clean_text(text):
+    text= str(text).lower()
+    text = " ".join([word.lower() for word in text.split() if word.lower() not in stop])
+    text = " ".join([i for i in text.split() if len(i) > 2])
+    return text
+
+def lower_case(text):
+    text= str(text).lower()
+    return text
+
+
+def split_into_sentences(text):
+    text = " " + text + "  "
+    text = text.replace("\n"," ")
+    text = re.sub(prefixes,"\\1<prd>",text)
+    text = re.sub(websites,"<prd>\\1",text)
+    text = re.sub(digits + "[.]" + digits,"\\1<prd>\\2",text)
+    if "..." in text: text = text.replace("...","<prd><prd><prd>")
+    if "Ph.D" in text: text = text.replace("Ph.D.","Ph<prd>D<prd>")
+    text = re.sub("\s" + alphabets + "[.] "," \\1<prd> ",text)
+    text = re.sub(acronyms+" "+starters,"\\1<stop> \\2",text)
+    text = re.sub(alphabets + "[.]" + alphabets + "[.]" + alphabets + "[.]","\\1<prd>\\2<prd>\\3<prd>",text)
+    text = re.sub(alphabets + "[.]" + alphabets + "[.]","\\1<prd>\\2<prd>",text)
+    text = re.sub(" "+suffixes+"[.] "+starters," \\1<stop> \\2",text)
+    text = re.sub(" "+suffixes+"[.]"," \\1<prd>",text)
+    text = re.sub(" " + alphabets + "[.]"," \\1<prd>",text)
+    if "”" in text: text = text.replace(".”","”.")
+    if "\"" in text: text = text.replace(".\"","\".")
+    if "!" in text: text = text.replace("!\"","\"!")
+    if "?" in text: text = text.replace("?\"","\"?")
+    text = text.replace(".",".<stop>")
+    text = text.replace("?","?<stop>")
+    text = text.replace("!","!<stop>")
+    text = text.replace("<prd>",".")
+    sentences = text.split("<stop>")
+    sentences = sentences[:-1]
+    sentences = [s.strip() for s in sentences]
+    return sentences
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World !!"}
+
+@app.post("/parse/url")
+async def parse_article(url):
+    data = Article(url)
+    data.download()
+    data.parse()
+    title = data.title
+    author = data.authors
+    text = data.text
+    text = remove_html(text)
+    text = clean_text(text)
+    source = urlparse(url).netloc
+    source = source.split('.')[1]
+    return {"source": source, "title": title, "author": author, "text":text}
+
+@app.post("/parse/extension")
+async def parse_article(url):
+    data = Article(url)
+    data.download()
+    data.parse()
+    title = data.title
+    author = data.authors
+    text = data.text
+    text = remove_html(text)
+    text = lower_case(text)
+    text = split_into_sentences(text)
+    return {"title": title, "author": author, "text":text}
+
